f5b08aa3f55a5f1b37aa4d5dad166b9ec1a53e64
Vrushank <raovrushank@gmail.com>
2024-09-14 13:58:00 +0530

    Made css changes

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI, Form
 from fastapi.staticfiles import StaticFiles
-import requests, io, base64
+import requests
+import io
+import base64
 from fastapi.responses import HTMLResponse
+from PIL import Image
+from io import BytesIO
 from dotenv import load_dotenv
 import os
 
@@ -27,15 +31,27 @@ async def text_to_image(text: str = Form(...)):
     }
 
     headers = {
-    'Content-Type': 'application/json',
-    'Authorization': f'Bearer {os.getenv("API_KEY")}'
-}
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {os.getenv("API_KEY")}'
+    }
 
     response = requests.post(API_URL, headers=headers, json=payload)
 
     if response.headers.get('Content-Type') == 'image/jpeg':
-        image_stream = io.BytesIO(response.content)
-        image_url = "data:image/jpeg;base64," + base64.b64encode(image_stream.getvalue()).decode('utf-8')
-        return HTMLResponse(f'<img src="{image_url}" alt="Generated Image"/>')
+        # Open the image with Pillow
+        image = Image.open(io.BytesIO(response.content))
+
+        # Resize the image to 400x400
+        image = image.resize((400, 400), Image.LANCZOS)
+
+        # Save resized image to a BytesIO object
+        buffered = BytesIO()
+        image.save(buffered, format="JPEG")
+        img_byte_array = buffered.getvalue()
+
+        # Encode the resized image as base64
+        image_url = "data:image/jpeg;base64," + base64.b64encode(img_byte_array).decode('utf-8')
+
+        return HTMLResponse(f'<div id="result-image-box"><img src="{image_url}" alt="Generated Image"/></div>')
     else:
         return HTMLResponse('<p>Error generating image</p>')
